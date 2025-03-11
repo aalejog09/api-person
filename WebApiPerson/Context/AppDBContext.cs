c39064e5964342a5a596f3955f08bbc17515159c
Andres Alejo <aalejog09@gmail.com>
2025-03-11 09:11:57 -0400

    commit final.
    API person basico

---
+++ WebApiPerson/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; } //coleccion de entidades
+
+    }
+}
